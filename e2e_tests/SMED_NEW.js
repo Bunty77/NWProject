711900736f6e06a927960582320f5381b0a12774
Navnath Sujgure <navnath.sujgure@springernature.com>
2018-10-10 14:49:03 +0530

    new file add for new course

---
+++ e2e_tests/SMED_NEW.js
@@ -0,0 +1,54 @@
+module.exports = {
+    'Verifying Login For SpringerMedizin ': function(browser) {
+      // Browser is the browser that is being controlled
+      browser
+        .url('https://nemo-sso-cas-server-staging.dev.cf.springer-sbm.com/login?wt_t=1538545752902&wt_eid=2153484227903755490&service=http%3A%2F%2Fnemo-core-staging-smed.dev.cf.private.springer.com%2Fcallback%3Fclient_name%3DCasClienthttp%3A%2F%2Fnemo-core-staging-smed.dev.cf.private.springer.com&locale=de&webtrekkId=846182373400841') // Navigate to the url
+        .waitForElementVisible('body', 1000) // Wait until you can see the body element.
+        
+        .setValue('input[name=username]','nemo.pune77@gmail.com')
+        .setValue('input[name=password]','Admin@12345')
+        .click('input[name=submit]')
+
+      },
+
+      'Verifying CME , List of all Course': function(browser)
+      {
+        browser
+        .useXpath()
+        .waitForElementVisible("//*[@class='main-navigation-teaser']/li[5]/a/span")
+        .click("//*[@class='main-navigation-teaser']/li[5]/a/span")
+        .waitForElementVisible(".//*[@id='main']/section[1]/div/div[1]/a/span[1]")
+        .pause(1000)
+        .click(".//*[@id='main']/section[1]/div/div[1]/a/span[1]")
+        .pause(2000)
+      },
+
+      'Verifying Course Title on Overview Page' : function(browser)
+      {
+        browser
+        .waitForElementVisible(".//*[@id='main']/section[3]/div[2]/section[1]/div/a/h3")
+        .click(".//*[@id='main']/section[3]/div[2]/section[1]/div/a/h3")
+        
+      },
+
+      'Verifying Course Start on Detail Page' : function(browser)
+      {
+        browser
+        .waitForElementVisible(".//*[@id='main']/div[3]/section/header/ul/li/a/span")
+        .click(".//*[@id='main']/div[3]/section/header/ul/li/a/span")
+        .pause(3000)
+        
+      },
+
+'Switch Window' : function(browser)
+{
+  browser	
+
+		.window_handles(function(result) {
+                var newHandle = result.value[1];
+                this.switchWindow(newHandle);
+            })
+            
+}
+
+};
