f5b9ffeef9585829325658de82184fd8e273cb95
Navnath Sujgure <navnath.sujgure@springernature.com>
2018-11-22 13:09:21 +0530

    sample test

--- e2e_tests/SmokeTest.js
+++ e2e_tests/SmokeTest.js
@@ -1,15 +1,10 @@
 module.exports = {
-    'Verifying Login For pflegenzuhause ': function(browser) {
+    'Title is Bing': function(browser) {
       // Browser is the browser that is being controlled
       browser
-        .url('https://www.pflegenzuhause.de/') // Navigate to the url
+        .url('https://www.springermedizin.de/') // Navigate to the url
         .waitForElementVisible('body', 1000) // Wait until you can see the body element.
-        browser.assert.equal(response.statusCode,200);
-        
-
+        .verify.title('Springer Medizin – Mein Arztwissen | springermedizin.de') // Verify that the title is 'Bing'
+        .end() // This must be called to close the browser at the end
       }
-
     };
-
-
-
